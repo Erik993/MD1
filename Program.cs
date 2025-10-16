3013c36a2b0427c0cdbe026f5a9020a78f9d0a4b
esokolovs <e.sokolovs97@gmail.com>
2025-10-16 22:15:50 +0300

    MD1 final

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Library;
+using Application;
+
+/*
+Abstract Class directory contains:
+* User abstract class
+* List abstract class to managa collections
+
+Data - directory created for json files
+
+Factories - contains methods to create specific objects
+
+Interfaces - IDatamanager interface
+
+Library - contains Assignment, Employee, ItSuppor, Ticket classes
+
+Repositories - collections for each type of object, that inherit from AbstractList.
+
+Services - JSON manager
+
+Application - Application data manager aggregates and manages all classes
+
+*/
+
+
+var dm = new ApplicationDataManager();
+
+// CreateTestData argument: integer is a count of test objects.
+dm.CreateTestData(1);
+dm.Print();
+dm.Save();
+dm.Reset();
+dm.Print();
+dm.Load();
+dm.Print();
+
